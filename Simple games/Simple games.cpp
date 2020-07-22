1412a16425c6176e00b6e0278f3a7c09e9560da0
Piotr Ciosmak <MrChick888@gmail.com>
2020-07-22 16:34:26 +0200

    Guesses_game_finished

--- Simple games/Simple games.cpp
+++ Simple games/Simple games.cpp
@@ -6,9 +6,12 @@
 #include <vector>
 #include <fstream>
 #include <Windows.h>
+#include <fstream>
 
 void leave_game();
-void print_guesses(std::vector<int> data);
+void main_menu();
+void best_score_guesses(std::vector<int>);
+void print_guesses(std::vector<int>);
 void play_guessing_game();
 
 char draw_color();
@@ -20,10 +23,7 @@ int main()
 	char choice;
 	while (true)
 	{
-		//menu function
-		std::cout << "0. Quit" << std::endl;
-		std::cout << "1. Play guessing game" << std::endl;
-		std::cout << "2. Play casino game" << std::endl;
+		main_menu();
 		choice = _getch();
 
 		switch (choice)
@@ -77,6 +77,38 @@ void leave_game()
 	}
 }
 
+void main_menu()
+{
+	std::cout << "0. Quit" << std::endl;
+	std::cout << "1. Play guessing game" << std::endl;
+	std::cout << "2. Play casino game" << std::endl;
+	std::cout << "Make your chooise: ";
+}
+
+void best_score_guesses(std::vector<int> data)
+{
+	std::ifstream guess_input("Guesses_best_score.txt");
+	int best_score;
+	guess_input >> best_score;
+	std::ofstream guess_output("Guesses_best_score.txt");
+	std::cout << "You won in " << data.size() << " moves." << std::endl;
+	if (data.size() < best_score)
+	{
+		guess_output << data.size();
+		std::cout << "It is a new best score! The old one was: " << best_score << std::endl << std::endl;
+	}
+	else if (data.size() == best_score)
+	{
+		guess_output << best_score;
+		std::cout << "Great job, You just align the best score (" << best_score << ")." << std::endl << std::endl;
+	}
+	else
+	{
+		guess_output << best_score;
+		std::cout << "So close, current best score: " << best_score << std::endl << std::endl;
+	}
+}
+
 void print_guesses(std::vector<int> data)
 {
 	for (int i = 0; i < data.size(); i++)
@@ -100,7 +132,8 @@ void play_guessing_game()
 		if (guess_numer == random_numer)
 		{
 			system("cls");
-			std::cout << guess_numer << " is correct. You win!" << std::endl;
+			std::cout << guess_numer << " is correct." << std::endl;
+			best_score_guesses(guesses);
 			std::cout << "Your guesses:" << std::endl;
 			print_guesses(guesses);
 			system("PAUSE");
@@ -156,30 +189,70 @@ void play_casino_game()
 
 	long double balans = 100;
 	save >> balans;
+
 	system("cls");
-	std::cout<<"Hello "<<player_name<<" "<<player_surname<<", You have " << balans << " credits." << std::endl;
+	std::cout << "Hello " << player_name << " " << player_surname << ", You have " << balans << " credits." << std::endl;
+
+	long double bet;
+
+	do
+	{
+		std::cout << "Enter an amout of credits to bet: ";
+		std::cin >> bet;
+		if (bet > balans)
+		{
+			std::cout << "You don't have enought credits! Try again" << std::endl;
+			system("PAUSE");
+			system("cls");
+		}
+		else if (bet <= 0)
+		{
+			std::cout << "You can't bet " << bet << " credits! Try again" << std::endl;
+			system("PAUSE");
+			system("cls");
+		}
+	} while (bet > balans || bet <= 0);
 
 	do
 	{
 		std::cout << "Pick the color (R|G|B): ";
 		std::cin >> player_color;
-		if ((player_color != 'R') && (player_color != 'G') && (player_color != 'B'))
+		std::toupper(player_color);//NIE DZIA£A
+		if (player_color != 'R' && player_color != 'G' && player_color != 'B')
 		{
 			std::cout << "This character is incorrect!" << std::endl;
 			system("PAUSE");
 			system("cls");
 		}
 
-	}while ((player_color != 'R') && (player_color != 'G') && (player_color != 'B'));
+	} while (player_color != 'R' && player_color != 'G' && player_color != 'B');
 
 	system("cls");
-	
+
 	for (int i = 5; i >= 0; i--)
 	{
 		std::cout << i;
 		Sleep(1000);
 		system("cls");
 	}
+
+	if (player_color = correct_color && correct_color == 'G')
+	{
+		std::cout << "Gratulacje You won " << 14 * bet << " credits" << std::endl;
+		balans += 13 * bet;
+	}
+	else if (player_color = correct_color && (correct_color == 'B' || correct_color == 'R'))
+	{
+		std::cout << "Gratulacje You won " << 2 * bet << " credits" << std::endl;
+		balans += bet;
+	}
+	else
+	{
+		std::cout << "Niestety You lost " << bet << " credits" << std::endl;
+		std::cout << "The correct color was " << correct_color << std::endl;
+		balans -= bet;
+	}
+	save << balans;
 	system("PAUSE");
 	system("cls");
 }
